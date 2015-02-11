cef31738a4d4ce72cba27af32340b290e3a05143
Sebastian Deiss <sdeiss@haw-landshut.de>
2015-02-11 12:10:44 +0100

    Improve the index site of the demo project

--- demo/views.py
+++ demo/views.py
@@ -6,7 +6,9 @@ Created on 16.12.2014
 
 
 from django.shortcuts import render
+from django_auth_qr2auth import __version__
 
 
 def index(request):
-    return render(request, 'demo/home.html')
+    return render(request, 'demo/home.html',
+                  {'Q2AVersion': __version__})
